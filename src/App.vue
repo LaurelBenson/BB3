59c1d98f4e13b7d94ad8a2f72fe46140632de906
LaurelBenson <laurel@twentyideas.com>
2019-11-16 20:42:06 -0800

    Added animate.css to main.js Added global.sass made sass work by updating sass loader
    Co-authored-by: Luca Ban <mesqueeb@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Hello I'm a BB"/>
+  <div id ="app">
+    <img class="animated infinite bounce delay-2s" alt="Vue logo" src="./assets/logo.png">
+    <div class="h1">Hello Laurel</div>
+    <HelloWorld msg ="Hey Luca!"/>
   </div>
 </template>
 
@@ -16,13 +17,15 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "css/global.sass"
+
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="hello">
     <h1 >{{ msg  }}</h1>
-    <p>
+    <p class="_p">
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3 class="animated infinite bounce delay-2s">Installed CLI Plugins</h3>
+    <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
@@ -40,20 +40,20 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style lang="sass" scoped>
+
+._p
+  color: $pink
+
+h3
+  margin: 40px 0 0
+ul
+  list-style-type: none
+  padding: 0
+li
+  display: inline-block
+  margin: 0 10px
+a
+  color: #42b983
 
 </style>
