88e657f9f6182197a47adefd4d9f022fec147d15
LaurelBenson <laurel@twentyideas.com>
2019-11-20 23:28:31 -0800

    Messing around on Friends page, animating the divs
    Co-authored-by: Luca Ban <mesqueeb@gmail.com>

--- src/views/Friends.vue
+++ src/views/Friends.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="friends">
+    <audio>
+      <source src="../assets/f.mp3" type="audio/mpeg">
+      <source src="../assets/f.wav" type="audio/wav">
+    </audio>
     <div class="header-text">
-      <h1 class="animated infinite bounce delay-2s">Hello friend</h1>
+      <h1 class="animated infinite bounce delay-2s">Hello {{ surprise ? 'Luca' : 'friend' }}</h1>
+      <div class="gif"></div>
+      <div
+        @mouseover="onMouseover"
+        @click="surprise = true"
+        v-if="!surprise"
+        id="trying"
+        class=""
+      ></div>
+      <div
+        v-if="surprise"
+      >
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
     </div>
     
   </div>
@@ -9,16 +26,55 @@
 
 <script>
 export default {
-    
+  data () {
+    return {
+      surprise: false,
+    }
+  },
+  methods: {
+    onMouseover () {
+      const audioEl = this.$el.getElementsByTagName('audio')[0]
+      audioEl.play()
+    },
+  },
 }
 </script>
 
 <style lang="sass">
 
-.friends
+@keyframes spin
+  0%
+    transform: rotate(0deg)
+  100%
+    transform: rotate(360deg)
+
+.header-text
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+.gif
   background-image: url("https://media.giphy.com/media/l0JLYJnQDnzSEoAOk/giphy.gif")
-  background-size: cover
-  width: 100vw
-  height: 100vh
+  background-size: contain
+  background-repeat: no-repeat
+  width: 300px
+  height: 300px
+  animation-name: spin
+  animation-duration: 5000ms
+  animation-iteration-count: infinite
+  animation-timing-function: linear 
+  transition: all 2s ease-in-out
+.gif:hover
+  transform: scale(2) !important
+
+#trying 
+  cursor: pointer
+  background-image: url("https://media.giphy.com/media/l0JLYJnQDnzSEoAOk/giphy.gif")
+  background-position: right
+  width: 300px
+  height: 300px
+  transition: all 300ms ease-in-out
+#trying:hover
+  transform: scale(2) !important
 
 </style>
