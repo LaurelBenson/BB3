5f119e4e37629bd1dee09bfb473acf4fb3e80076
LaurelBenson <laurel@twentyideas.com>
2019-11-21 21:54:38 -0800

    Added components through main.js !

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 import 'animate.css/animate.min.css'
 import router from './router'
 
+Vue.component('greeting', {
+  template: '<p>hey there, Im a {{ name }}.     <button v-on:click="changeName" class="animated wobble 1s delay-2s infinite">Change Name</button></p>',
+  data: function(){
+    return {
+      name: "Yoshi"
+    }
+  },
+  methods: {
+    changeName: function (){
+      this.name = 'Mario';
+    }
+  }
+}); 
+
+new Vue ({
+  el:"vue-app-one"
+});
+
+new Vue ({
+  el:"vue-app-one"
+});
+
+
 Vue.config.productionTip = false
 
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is a cute lil about page</h1>
+    <div id="vue-app-one">
+      <h2>Vue App Uno</h2>
+      <greeting></greeting>
+    </div>
+
+    <div id="vue-app-two">
+        <h2>Vue App Dos</h2>
+        <greeting></greeting>
+    </div>
+
+
   </div>
 </template>
